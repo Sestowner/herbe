8ffd77a1fe981f1e29d7ca1aa018f574ff413ff7
Sergiy Stupar <owner@sestolab.pp.ua>
2021-11-08 11:41:54 +0200

    Add config.h

---
+++ config.h
@@ -0,0 +1,20 @@
+static const char *background_color = "#073642";
+static const char *border_color = "#93a1a1";
+static const char *font_color = "#657b83";
+static const char *font_pattern = "monospace:size=12";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 15;
+
+static const unsigned int width = 500;
+static const unsigned int border_size = 3;
+static const unsigned int pos_x = 0;
+static const unsigned int pos_y = 0;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 5; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
+
